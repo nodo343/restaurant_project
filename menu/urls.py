67ada211b112ae62158ac481be8488a9476ad3e3
nodo343 <nodikogamer01@gmail.com>
2026-09-03 13:00:46 +0400

    Update urls.py

--- menu/urls.py
+++ menu/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.dish_list, name='dish_list'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
     path('cart/', views.cart_detail, name='cart_detail'),
     path('cart/add/<int:dish_id>/', views.add_to_cart, name='add_to_cart'),
     path('cart/update/<int:dish_id>/', views.update_cart_item, name='update_cart_item'),
