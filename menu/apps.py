6696d4aeebeecb8b163ac5aa4975f1688a287ada
nodo343 <nodikogamer01@gmail.com>
2026-09-03 13:37:51 +0400

    Update apps.py

--- menu/apps.py
+++ menu/apps.py
@@ -5,3 +5,19 @@ class MenuConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'menu'
     verbose_name = 'რესტორნის მენიუ'
+
+    def ready(self):
+        from django import VERSION as DJANGO_VERSION
+
+        if DJANGO_VERSION >= (5, 2, 8):
+            return
+
+        from django.template.context import BaseContext
+
+        def copy_context(context):
+            duplicate = context.__class__.__new__(context.__class__)
+            duplicate.__dict__.update(context.__dict__)
+            duplicate.dicts = context.dicts[:]
+            return duplicate
+
+        BaseContext.__copy__ = copy_context
